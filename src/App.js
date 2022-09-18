014b59d0de527bf9acc426a1273a35262c27913b
Pat Lawler <patlaw777@icloud.com>
2022-09-18 18:14:16 -0500

    refactored

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ function App() {
   };
 
   const saveData = (dataSet) => {
-    // converts and saves the passed in data set to localstorage
+    // sets the datatset in state
     setData(dataSet);
+
+    // converts and saves the current datatset to localstorage
     localStorage.setItem('airplanes', JSON.stringify(dataSet));
   };
 
@@ -31,38 +33,37 @@ function App() {
     let newDataSet;
 
     if(!data) { 
-      // if no previous data is stored it will create a blank data set
+      // if no previous data is stored it will create a blank datatset
       newDataSet = []; 
     }
     else {
-      // copies the existing data to a new data set
+      // copies the existing data into a new datatset
       newDataSet = data.filter((e) => { return true; });
     }
     
     // adds the new airplane
     newDataSet.push(plane);
 
-    // stores and updates the data set
+    // stores and updates the datatset
     saveData(newDataSet);
   };
 
   const deleteAirplane = (index) => {
-    // copies the existing data to a new data set without the specified record
+    // copies the existing data to a new datatset without the specified record
     const newDataSet = data.filter((e, i) => { return index !== i; });
 
-    // stores and updates the data set
+    // stores and updates the datatset
     saveData(newDataSet);
   };
 
   const updateAirplane = (plane, index) => {
-    
-    // copies the existing data to a new data set
+    // copies the existing data to a new datatset
     const newDataSet = data.filter((e, i) => { return true; });
 
     // updates the speciied location with the new plane information
     newDataSet.splice(index, 1, plane);
 
-    // stores and updates the data set
+    // stores and updates the datatset
     saveData(newDataSet);
   };
 
@@ -70,27 +71,28 @@ function App() {
   // HANDLER FUNCTIONS
 
   const buttonClick = (e) => {
-    // button handler for the non-modal buttons, the classname is used to determine which button was pressed.
+    // button handler for the non-modal buttons, the classname is used to determine which button was tapped
     
     // disables button functionality if the modal is being displayed
     if (showModal) return;
 
-    
-    // if an edit or delete is pressed, then the index of the data is extrapolated from the element.target data attribute to identify which record to be edited or deleted.
+    // if edit or delete are tapped the index of the data record is extrapolated from the target's data attribute
     const itemNumber = parseInt(e.target.dataset.item);
 
     switch (e.target.className) {
       case 'add_button':
         // ensures the editing record state is null, so the modal displays add airplane
         setRecord(null);
-        // shows the display modal form
+        
+        // displays the modal form
         displayModal(true);
         break;
 
       case 'edit_button':
         // sets the editing record state to the index number of the item that is to be edited
         setRecord(itemNumber);
-        // sows the display modal form
+        
+        // displays the modal form
         displayModal(true);
         break;
 
@@ -103,7 +105,7 @@ function App() {
 
   // STATE
 
-  // stores the airplane array
+  // stores the airplane array, gets all airplanes initially
   const [data, setData] = useState(getAllAirplanes());
 
   // toggles the modal page on/off
@@ -118,7 +120,7 @@ function App() {
       <h1 className='page_title'>Joe's Airplanes</h1>
 
       <main>
-        {/* if there are aircraft in the data set then display the rows */}
+        {/* if there are aircraft in the datatset then display the rows */}
         {(data && data.length !==0) && (
           <>
             {/* creates the labels at the top of the rows */}
@@ -130,14 +132,14 @@ function App() {
               <div></div>
             </div>
 
-            {/* uses the Item componenet to create an invidual row for each airplane in the dataset */}
+            {/* uses the Item componenet to create an invidual row for each airplane in the datatset */}
             {data.map((plane, i) => (
               <Item key={i} plane={plane} buttonClick={buttonClick} index={i} />
             ))}
           </>
         )}
 
-        {/* if there are no aircraft in the data set then display the following */}
+        {/* if there are no aircraft in the datatset then display the following */}
         {(!data || data.length === 0) && <div>No Airplanes have been saved yet!</div>}
 
         {/* provides the add button to add aircraft to the dataset */}
